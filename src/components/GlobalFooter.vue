1bfe942621cf89431249dc660c2309b58b3dcbfb
yusakuOono <black.radio1234@gmail.com>
2020-03-18 18:39:16 +0900

    GlobalFooter

--- src/components/GlobalFooter.vue
+++ src/components/GlobalFooter.vue
@@ -1,7 +1,7 @@
 <template>
 <footer class="footer">
   <p class="backTop">
-    <a href="/" class="router-link-exact-active router-link-active">最初からやりなおす</a>
+    <a href="/covid19-diagnoser/" class="router-link-exact-active router-link-active">最初からやりなおす</a>
   </p>
 </footer>
 </template>
